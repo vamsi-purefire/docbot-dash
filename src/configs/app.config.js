e5ef1a0274d4379f871dd8862bb5f4051fa910b1
vamsijanaki <itsvamsij@gmail.com>
2022-11-20 21:03:49 +0530

    Disable Mock

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -3,7 +3,7 @@ const appConfig = {
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
     tourPath: '',
-    enableMock: true
+    enableMock: false
 }
 
 export default appConfig
